e6b709434d7a30a92da6185c532f0490175a34fb
Matthew Jordan <matthewjordandevops@yandex.com>
2016-05-21 11:45:55 -0400

    Move el-get installation to a script.
    * Packages.el: Modified file.

--- Packages.el
+++ Packages.el
@@ -1,24 +1,7 @@
 (unless (executable-find "guix")
 	(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-	(unless
-			(require 'el-get nil 't)
-
-		(require 'package)
-		(add-to-list 'package-archives
-								 '("melpa" . "http://melpa.org/packages/"))
-
-		(package-refresh-contents)
-		(package-initialize)
-		(package-install 'el-get)
-		(require 'el-get))
-
-	(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-	(el-get 'sync)
-
-	;; (unload-feature 'el-get)
-	;; (require 'el-get)
-
+	(require 'el-get)
 	(setq package-list
 				'(;; general utilities
 					el-get
