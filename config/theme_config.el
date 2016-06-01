a9db4d88a30256eb7198d7481daffdae4fb08a0e
Matthew Jordan <matthewjordandevops@yandex.com>
2016-05-31 20:59:26 -0400

    color-theme is not needed.
    * config/theme_config.el: Modified file.

--- config/theme_config.el
+++ config/theme_config.el
@@ -1,12 +1,7 @@
-(when
-		(require 'color-theme nil 'noerror)
 
-	(color-theme-initialize)
-	(setq color-theme-is-global t)
-
-	(if (require 'birds-of-paradise-plus-theme nil 'noerror)
-			(progn
-				(load-theme 'birds-of-paradise-plus t))
-
+(if (require 'birds-of-paradise-plus-theme nil 'noerror)
 		(progn
-			(load-theme 'tango-dark t))))
+			(load-theme 'birds-of-paradise-plus t))
+
+	(progn
+		(load-theme 'tango-dark t)))
