466cff4d52ae1885a0a5e2388a91d9b88d41f255
Matthew Jordan <matthewjordandevops@yandex.com>
2016-08-27 16:02:52 -0400

    Added elpa.

--- Packages.el
+++ Packages.el
@@ -2,6 +2,11 @@
 	(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
 	(require 'el-get)
+	(require 'el-get-elpa)
+
+	(unless (file-directory-p el-get-recipe-path-elpa)
+		(el-get-elpa-build-local-recipes))
+
 	(setq package-list
 				'(;; general utilities
 					el-get
